4294229d40a49fa5ca42b4ed8baec2dc7eacea43
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-08-30 15:12:28 +0200

    Display current time in Timer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Timer from './Timer'
 
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Timer />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: new Date()
+    }
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID)
+  }
+
+  tick() {
+    this.setState({ time: new Date() })
+  }
+
+  padTo2with0(text) {
+    text = text.toString()
+    if (text.length === 1) return '0' + text
+    return text
+  }
+
+  render() {
+    const today = this.state.time
+    const time = `${this.padTo2with0(today.getHours())}:${this.padTo2with0(
+      today.getMinutes()
+    )}:${this.padTo2with0(today.getSeconds())}`
+
+    return (
+      <div className="App">
+        <Timer time={time} />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/Timer.js
+++ src/Timer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import './Timer.css'
 
-const Timer = () => (
+const Timer = ({ time }) => (
   <div className="Timer circle">
-    <p className="hour-display">16:36:52</p>
+    <p className="hour-display">{time}</p>
   </div>
 )
 
