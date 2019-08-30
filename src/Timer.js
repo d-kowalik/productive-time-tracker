3dd902f5ecace17a97b6e5bd8d0585091df6638f
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-08-30 14:49:23 +0200

    Initial commit

---
+++ src/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Timer.css'
+
+const Timer = () => (
+  <div className="Timer circle">
+    <p className="hour-display">16:36:52</p>
+  </div>
+)
+
+export default Timer
