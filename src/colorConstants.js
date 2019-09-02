94095893e9ad17593707023394aacd28ecb33d48
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-09-02 16:31:43 +0200

    Add color constants to colorConstants.js

---
+++ src/colorConstants.js
@@ -0,0 +1,9 @@
+export const SLEEP_COLOR = '#add8b6'
+export const WORK_COLOR = '#ffcccb'
+export const PLAY_COLOR = '#fed8b1'
+
+export const TaskStateAsColor = {
+  SLEEP: SLEEP_COLOR,
+  WORK: WORK_COLOR,
+  PLAY: PLAY_COLOR
+}
