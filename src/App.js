c23dd5ddc3789aff773560e05ec19762fd298c0a
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-09-02 19:53:00 +0200

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { withCookies } from 'react-cookie'
 import Timer from './Timer'
 import DynamicCircle from './DynamicCircle'
 import TaskSelector from './TaskSelector'
-import { SLEEP_COLOR, WORK_COLOR, PLAY_COLOR } from './colorConstants'
 import { todayDMY } from './dateHelper'
 
 import './App.css'
