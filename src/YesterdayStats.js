7cac1e43dd98662d0146718e531d44551d1360d1
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-09-06 20:22:50 +0200

    Fix combineIntoPercentages

--- src/YesterdayStats.js
+++ src/YesterdayStats.js
@@ -2,17 +2,27 @@ import React from 'react'
 
 const combineIntoPercentages = states => {
   const res = {}
+  let lastPercent = 0
+  let lastState
+
   for (const [k, v] of Object.entries(states)) {
-    if (res[k] === undefined) res[k] = v
-    else res[k] += v
+    if (res[v] === undefined) res[v] = k - lastPercent
+    else res[v] += k - lastPercent
+    lastPercent = k
+    lastState = v
   }
+  res[lastState] += 100 - lastPercent
   return res
 }
 
-const YesterdayStats = ({ taskStates }) => (
-  <div className="YesterdayStats">
-    <h3>Yesterday, you spent..</h3>
-  </div>
-)
+const YesterdayStats = ({ taskStates }) => {
+  console.log(combineIntoPercentages(taskStates))
+
+  return (
+    <div className="YesterdayStats">
+      <h3>Yesterday, you spent..</h3>
+    </div>
+  )
+}
 
 export default YesterdayStats
